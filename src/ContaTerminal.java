62e59f141631648afedcf90551bcbe665919fcb1
Hugo Teixeira <hugots23@gmail.com>
2024-04-25 17:53:22 -0300

    chore: todas as pendências finalizadas. Desafio feito e utilizando novos aprendizados

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,12 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        // TODO: conhecer e importar a classe Scanner
+        Scanner sc = new Scanner(System.in);
+        Cliente cliente = new Cliente();
+        Conta conta = new Conta();
+        conta.setCliente(cliente);
 
-        // Exibir as mensagens p/ o nosso usuário
+        System.out.println("Seja bem vindo(a) ao processo de criação de Conta! \n" +
+                "Preencha os dados conformem forem aparecendo no questionário. \n");
 
-        // Obter pela Scanner os valores digitados no terminal
+        System.out.println("Por favor, digite seu nome:");
+        cliente.setNome(sc.nextLine());
 
-        // Exibir a mensagem conta criada
+        System.out.println("Agora, digite o número da conta:");
+        conta.setNumero(sc.nextInt());
 
+        System.out.println("E por fim, digite o número da agência:");
+        conta.setAgencia(sc.next());
+
+        conta.setSaldo(50);
+
+        System.out.println("-----------------------------------------");
+        System.out.println("Olá, "+ conta.getCliente().getNome() +"! Obrigado por criar uma conta em nosso banco. \n" +
+                        "Sua agência é: " + conta.getAgencia() + "\n" +
+                        "Seu número de conta é: " + conta.getNumero() + "\n" +
+                        "O saldo inicial de R$" + conta.getSaldo() + " já está disponível para saque.");
     }
 }
