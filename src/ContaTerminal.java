786990a389f509fe9b06c32d3a03d6a5b5009ba0
Hugo Teixeira <hugots23@gmail.com>
2024-04-25 16:17:02 -0300

    chore: comentários de passo a passo adicionado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,12 @@
 public class ContaTerminal {
     public static void main(String[] args) {
-        
+        // TODO: conhecer e importar a classe Scanner
+
+        // Exibir as mensagens p/ o nosso usuário
+
+        // Obter pela Scanner os valores digitados no terminal
+
+        // Exibir a mensagem conta criada
+
     }
 }
